f6edb3171e3a3b1fe56ec0de0b98b15f9ca7ed7c
Fred Sauer <fredsa@google.com>
2024-04-28 10:03:46 -0700

    Basic function calling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, viewChild } from '@angular/core';
 import { LogService as LogService } from './log.service';
-import { GoogleGenerativeAI } from '@google/generative-ai';
+import { FunctionCallingConfig, FunctionCallingMode, FunctionDeclaration, FunctionDeclarationSchema, FunctionDeclarationSchemaProperty, FunctionDeclarationSchemaType, GenerativeModel, GoogleGenerativeAI, ToolConfig } from '@google/generative-ai';
 
 @Component({
   selector: 'app-root',
@@ -28,13 +28,88 @@ export class AppComponent {
         this.log.error('API Key must be provided');
         return;
       }
-      
+
+      const tableColumnProperties: { [k: string]: FunctionDeclarationSchemaProperty } = {
+        columnName: {
+          type: FunctionDeclarationSchemaType.STRING,
+          nullable: false,
+          description:
+            "Column name. Column names should be lowercase, and use snake_case.",
+        },
+        columnType: {
+          type: FunctionDeclarationSchemaType.STRING,
+          nullable: false,
+          enum: ["string", "integer", "date",],
+          description:
+            "Column type. Specifies the type of data that can be stored in this column.",
+        },
+      };
+
+      const tableColumnSchema: FunctionDeclarationSchema = {
+        type: FunctionDeclarationSchemaType.OBJECT,
+        description: "Table column. Specifies the properties of a table column.",
+        properties: tableColumnProperties,
+        required: ["columnName, columnType"],
+      };
+
+      const tableProperties: { [k: string]: FunctionDeclarationSchemaProperty } = {
+        tableName: {
+          type: FunctionDeclarationSchemaType.STRING,
+          nullable: false,
+          description:
+            "Table name. Table names should be lowercase, and use snake_case.",
+        },
+        columns: {
+          type: FunctionDeclarationSchemaType.ARRAY,
+          nullable: false,
+          description: "Array of table columns definitions.",
+          items: tableColumnSchema,
+        },
+      };
+
+      const createTableFunctionDeclaration: FunctionDeclaration = {
+        name: "createTable",
+        parameters: {
+          type: FunctionDeclarationSchemaType.OBJECT,
+          description: "Create database table.",
+          properties: tableProperties,
+          required: ["tableName", "columns"],
+        },
+      };
+
       const genAI = new GoogleGenerativeAI(apiKey);
       const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro-latest" });
+
+      model.tools = [{ functionDeclarations: [createTableFunctionDeclaration] }];
+
+      model.toolConfig = {
+        functionCallingConfig: {
+          // Require function calling response.
+          // mode: FunctionCallingMode.ANY,
+          // allowedFunctionNames: ["createTable"],
+        }
+      };
+
+      model.systemInstruction = {
+        role: "user",
+        parts: [{
+          text: "You are 'CRUDy bot', an AI database agent." +
+            " Users write basic CRUD (Create, Read, Update, Delete) operations in plain text." +
+            " For each such statement, you determine a suitable database table structure." +
+            " You respond with the a function call that would create that table strucutre."
+        }],
+      };
+
       const result = await model.generateContent(this.prompt().nativeElement.value);
       const response = await result.response;
-      const text = response.text();
-      this.log.info("response:" + text);
+      this.log.info("response:" + response);
+      const calls = response.functionCalls();
+      if (calls) {
+        this.log.info("response.functionCalls():", calls);
+      }
+      if (response.text()) {
+        this.log.info("response.text():", response.text());
+      }
     } catch (e) {
       this.log.catch(e)
     }
--- src/app/log.service.ts
+++ src/app/log.service.ts
@@ -23,20 +23,31 @@ export class LogService {
 
   constructor() { }
 
-  debug(msg: string) {
-    this.logMessages.push({ level: Level.debug, datetime: new Date(), message: msg, error: undefined });
+  totext(...args: any[]): string {
+    console.log("args", args);
+
+    return args.reduce((prev: any, curr: any, i: number, arr: any[]) => {
+      console.log("prev", prev, "curr", curr);
+      prev = prev ? prev + " " : "";
+      prev += JSON.stringify(curr, null, 2)
+      return prev;
+    });
   }
 
-  info(msg: string) {
-    this.logMessages.push({ level: Level.info, datetime: new Date(), message: msg, error: undefined });
+  debug(...args: any[]) {
+    this.logMessages.push({ level: Level.debug, datetime: new Date(), message: this.totext(...args), error: undefined });
   }
 
-  warn(msg: string) {
-    this.logMessages.push({ level: Level.warn, datetime: new Date(), message: msg, error: undefined });
+  info(...args: any[]) {
+    this.logMessages.push({ level: Level.info, datetime: new Date(), message: this.totext(...args), error: undefined });
   }
 
-  error(msg: string) {
-    this.logMessages.push({ level: Level.error, datetime: new Date(), message: msg, error: undefined });
+  warn(...args: any[]) {
+    this.logMessages.push({ level: Level.warn, datetime: new Date(), message: this.totext(...args), error: undefined });
+  }
+
+  error(...args: any[]) {
+    this.logMessages.push({ level: Level.error, datetime: new Date(), message: this.totext(...args), error: undefined });
   }
 
   catch(err: any) {
