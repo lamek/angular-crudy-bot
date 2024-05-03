5f1aaacf19a190d0ce87ad05d24eb28171e11216
Fred Sauer <fredsa@google.com>
2024-05-02 21:12:21 -0700

    default to using system instruction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,7 @@ export class AppComponent {
     "Remember anniversary for April Fools day",
   ];
 
-  useSystemInstruction = false;
+  useSystemInstruction = true;
 
   systemInstruction = `You are an AI database agent.
 
