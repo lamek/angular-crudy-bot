18e8583f8cd666d16f49323f9b84beb5e759d4b5
Fred Sauer <fredsa@google.com>
2024-05-05 20:59:42 -0700

    fix section css

--- src/app/gemini-response/gemini-response.component.ts
+++ src/app/gemini-response/gemini-response.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { GeminiService } from '../gemini.service';
 import { LogService } from '../log.service';
+import { HelpfulLabelComponent } from '../helpful-label/helpful-label.component';
 
 @Component({
   selector: 'app-gemini-response',
   standalone: true,
-  imports: [],
+  imports: [HelpfulLabelComponent],
   templateUrl: './gemini-response.component.html',
   styleUrl: './gemini-response.component.css'
 })
